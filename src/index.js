7a7214d6e3ed84bc7ff4e23e71841dcbd40a4559
Nearh777 <nearh777@gmail.com>
2023-04-01 13:07:52 +0300

    finish styles form

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './sass/index.scss';
+//import NewsApiService from './js/api-service';
+import { lightbox } from './js/lightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
